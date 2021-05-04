7be5a1301b25eb0200f899868d8d96f78ed1948d
vladprotsenko <59956384+vladprotsenko@users.noreply.github.com>
2021-05-04 15:53:05 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo gunicorn -bind='0.0.0.0:8080'hello:app
