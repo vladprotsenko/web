b164da00ffda16042a531458c6289f60faa301aa
vladprotsenko <59956384+vladprotsenko@users.noreply.github.com>
2021-05-24 10:22:54 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def app(environ, start_response):
-    data = b"Hello, World!\n"
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
     ])
-    return iter([data]
+    return body
