8b416c53a1412e41cef117b15d6e985439d59d3a
vladprotsenko <vl@granit.kz>
2021-05-28 08:58:17 +0000

    stepik

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn -b 0.0.0.0:8080 hello:app
-sudo /etc/init.d/gunicorn restart
+gunicorn -b 0.0.0.0:8080 hello:app
+
